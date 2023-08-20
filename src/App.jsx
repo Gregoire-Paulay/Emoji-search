0b74d6a7aaedba3b67d2f77e95162990b51f3071
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-08-20 17:29:16 +0200

    Projet fini + ajout fonction copié par emoji

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,40 @@ import Footer from "./components/Footer";
 const App = () => {
   const [search, setSearch] = useState("");
 
+  const emojiSelection = [];
+  for (let i = 0; i < data.length; i++) {
+    const emoji = data[i];
+    if (emojiSelection.length < 20) {
+      if (emoji.keywords.includes(search)) {
+        emojiSelection.push(
+          <button
+            key={emoji.title}
+            onClick={() => {
+              navigator.clipboard.writeText(emoji.symbol);
+            }}
+          >
+            {emoji.symbol} {emoji.title}
+          </button>
+        );
+      }
+    } else {
+      break;
+    }
+  }
+
   return (
     <>
-      <header>
-        <h1>🤫 Emoji Search 🤫</h1>
-        <input
-          value={search}
-          type="text"
-          onChange={(event) => {
-            setSearch(event.target.value);
-            data.map((emoji) => {
-              // console.log(emoji.keywords);
-            });
-          }}
-        />
-      </header>
-      <main>
-        {data.map((emoji) => {
-          return (
-            <div key={emoji.keywords}>
-              <button>
-                <p>{emoji.title} </p>
-                <p>{emoji.symbol}</p>
-              </button>
-            </div>
-          );
-        })}
-      </main>
+      <h1>🤫 Emoji Search 🤫</h1>
+      <input
+        type="text"
+        placeholder="Which emoji are you looking for ?"
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+
+      <div className="container">{emojiSelection}</div>
+
       <Footer />
     </>
   );
